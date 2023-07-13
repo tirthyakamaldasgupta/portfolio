f744aed4e02fcc15369b703e591cbf7a432d7391
tirthyakamaldasgupta <dasguptatirthyakamal@gmail.com>
2023-07-13 07:47:23 +0530

    fix: Fixed breakpoint issue in showing email and phone buttons and address header, but need to test on separate screen sizes

--- src/app/components/client/Connect.tsx
+++ src/app/components/client/Connect.tsx
@@ -1,10 +1,14 @@
-import { Box, Button, Card, CardBody, CardHeader, HStack, Heading, Link, SimpleGrid, Tag, TagCloseButton, TagLabel, Text } from "@chakra-ui/react";
+import { Box, Button, Card, CardBody, CardHeader, Flex, FormControl, FormErrorMessage, FormHelperText, FormLabel, HStack, Heading, Input, Link, SimpleGrid, Stack, Tag, TagCloseButton, TagLabel, Text, useBreakpointValue, useMediaQuery } from "@chakra-ui/react";
 import { faLinkedin, faMedium, faHashnode, faGithub, faYoutube } from "@fortawesome/free-brands-svg-icons";
-import { faArrowRight, faUpRightFromSquare } from "@fortawesome/free-solid-svg-icons";
+import { faArrowRight, faL, faUpRightFromSquare } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import Image from "next/image";
+import { SetStateAction, useState } from "react";
+import { isError } from "util";
 
 export default function Connect() {
+    const isMaxScreen = useBreakpointValue({ base: true, md: true, lg: true, xl: false });
+
     const socialAccounts = [
         {
             icon: faLinkedin,
@@ -33,6 +37,20 @@ export default function Connect() {
         }
     ];
 
+    // const [firstName, setFirstName] = useState("");
+    // const [lastName, setLastName] = useState("");
+    // const [email, setEmail] = useState("");
+    // const [companyName, setCompanyName] = useState("");
+    // const [message, setMessage] = useState("");
+
+    // const handleFirstNameChange = (e: { target: { value: SetStateAction<string>; }; }) => setFirstName(e.target.value);
+    // const handleLastNameChange = (e: { target: { value: SetStateAction<string>; }; }) => setLastName(e.target.value);
+    // const handleEmailChange = (e: { target: { value: SetStateAction<string>; }; }) => setEmail(e.target.value);
+    // const handleCompanyNameChange = (e: { target: { value: SetStateAction<string>; }; }) => setCompanyName(e.target.value);
+    // const handleMessageChange = (e: { target: { value: SetStateAction<string>; }; }) => setMessage(e.target.value);
+
+    // const isFirstNameError = input === ''
+
     return (
         <>
             <SimpleGrid columns={[1, 2]} spacing={1} padding={4}>
@@ -44,17 +62,38 @@ export default function Connect() {
             <SimpleGrid columns={[1, 2]} spacing={10} padding={4}>
                 <Box>
                     <Heading size={"sm"} marginBottom={5}>Drop me an email on</Heading>
-                    <Button as={Link} href="mailto:dasguptatirthyakamal@gmail.com" marginRight={2} colorScheme="red" textDecoration={"none"} _hover={{ textDecoration: "none" }}>dasguptatirthyakamal@gmail.com</Button>
-                    <Button as={Link} href="mailto:dasguptatirthyakamal@outlook.com" marginRight={2} colorScheme="blue" textDecoration={"none"} _hover={{ textDecoration: "none" }}>dasguptatirthyakamal@outlook.com</Button>
+                    <Flex direction={isMaxScreen ? "column" : "row"} flexWrap={isMaxScreen ? "wrap" : "nowrap"}>
+                        <Flex direction="column" marginRight={!isMaxScreen ? 2 : 0} marginBottom={isMaxScreen ? 2 : 0}>
+                            <Button as={Link} href="mailto:dasguptatirthyakamal@gmail.com" width={isMaxScreen ? "100%" : "auto"} colorScheme="red" textDecoration={"none"} _hover={{ textDecoration: "none" }}>dasguptatirthyakamal@gmail.com</Button>
+                        </Flex>
+                        <Flex direction="column" marginRight={!isMaxScreen ? 2 : 0} marginBottom={isMaxScreen ? 2 : 0}>
+                            <Button as={Link} href="mailto:dasguptatirthyakamal@outlook.com" width={isMaxScreen ? "100%" : "auto"} colorScheme="blue" textDecoration={"none"} _hover={{ textDecoration: "none" }}>dasguptatirthyakamal@outlook.com</Button>
+                        </Flex>
+                    </Flex>
 
                     <Heading size={"sm"} marginY={5}>Call or WhatsApp me on</Heading>
-                    <Button as={Link} href="tel:+8240603916" marginRight={2} colorScheme="green" textDecoration={"none"} _hover={{ textDecoration: "none" }}>+8240603916</Button>
-                    <Button as={Link} href="tel:+8017352824" marginRight={2} colorScheme="green" textDecoration={"none"} _hover={{ textDecoration: "none" }}>+8017352824</Button>
+                    <Flex direction={isMaxScreen ? "column" : "row"} flexWrap={isMaxScreen ? "wrap" : "nowrap"}>
+                        <Flex direction="column" marginRight={!isMaxScreen ? 2 : 0} marginBottom={isMaxScreen ? 2 : 0}>
+                            <Button as={Link}
+                                href="tel:+8240603916" width={isMaxScreen ? "100%" : "auto"} colorScheme="green" textDecoration="none" _hover={{ textDecoration: "none" }} >
+                                +8240603916
+                            </Button>
+                        </Flex>
+                        <Flex direction="column" marginRight={!isMaxScreen ? 2 : 0} marginBottom={isMaxScreen ? 2 : 0}>
+                            <Button as={Link} href="tel:+8017352824" width={isMaxScreen ? "100%" : "auto"} colorScheme="green" textDecoration="none" _hover={{ textDecoration: "none" }} >
+                                +8017352824
+                            </Button>
+                        </Flex>
+                    </Flex>
 
-                    <HStack>
-                        <Heading size={"sm"} marginY={5}>If you live nearby, we can always sit over a cup of</Heading>
-                        <Image src={"/coffee-cup.png"} alt="Coffee cup image" width={20} height={20}></Image>
-                        <Heading size={"sm"} marginY={5}>at</Heading>
+                    <HStack wrap={isMaxScreen ? "wrap" : "nowrap"} align="center" spacing={2}>
+                        <Heading size={isMaxScreen ? "xs" : "sm"} marginY={5}>
+                            If you live nearby, we can always sit over a cup of
+                        </Heading>
+                        <Image src="/coffee-cup.png" alt="Coffee cup image" width={20} height={20} />
+                        <Heading size={isMaxScreen ? "xs" : "sm"} marginY={5}>
+                            at
+                        </Heading>
                     </HStack>
 
                     <Text>Block B and C, New Tollygunge, Aurobindo Park, South Kolkata, West Bengal 700093</Text>
@@ -68,9 +107,26 @@ export default function Connect() {
                     ))}
                 </Box>
 
-                <Card borderRadius={10}>
-
-                </Card>
+                <Box>
+                    <FormControl isRequired paddingBottom={4}>
+                        <Input type="text" variant="flushed" placeholder="First name *" _placeholder={{ color: "inherit", fontWeight: "bold" }} />
+                    </FormControl>
+                    <FormControl isRequired paddingBottom={4}>
+                        <Input type="text" variant="flushed" placeholder="Last name *" _placeholder={{ color: "inherit", fontWeight: "bold" }} />
+                    </FormControl>
+                    <FormControl isRequired paddingBottom={4}>
+                        <Input type="email" variant="flushed" placeholder="Email *" _placeholder={{ color: "inherit", fontWeight: "bold" }} />
+                    </FormControl>
+                    <FormControl paddingBottom={4}>
+                        <Input type="text" variant="flushed" placeholder="Company" _placeholder={{ color: "inherit", fontWeight: "bold" }} />
+                    </FormControl>
+                    <FormControl isRequired paddingBottom={4}>
+                        <Input type="text" variant="flushed" placeholder="Message *" _placeholder={{ color: "inherit", fontWeight: "bold" }} />
+                    </FormControl>
+                    <FormControl paddingBottom={4}>
+                        <Button type="button"><Text>Submit enquiry</Text><FontAwesomeIcon icon={faArrowRight} style={{ marginLeft: "5px" }} /></Button>
+                    </FormControl>
+                </Box>
             </SimpleGrid>
         </>
     )
