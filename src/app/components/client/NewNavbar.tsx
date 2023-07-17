9c7ed846e1d93dd92065aeaba871fafa11500757
tirthyakamaldasgupta <dasguptatirthyakamal@gmail.com>
2023-07-17 17:39:00 +0530

    feat: Included a new sticky navbar

---
+++ src/app/components/client/NewNavbar.tsx
@@ -0,0 +1,97 @@
+import {
+    Button,
+    ButtonGroup,
+    Drawer,
+    DrawerBody,
+    DrawerContent,
+    DrawerHeader,
+    DrawerOverlay,
+    Flex,
+    Heading,
+    IconButton,
+    Spacer,
+    useBreakpointValue,
+    useDisclosure
+} from "@chakra-ui/react";
+import {HamburgerIcon} from "@chakra-ui/icons";
+import {Quicksand} from "next/font/google";
+
+const quicksand = Quicksand({
+    weight: "600",
+    subsets: ["latin"],
+});
+
+export default function NewNavbar() {
+    const items = ["EXPERIENCE", "PROJECTS", "EDUCATION", "BLOGS", "CONNECT"];
+
+    const {isOpen, onOpen, onClose} = useDisclosure();
+
+    const displayDesktop = useBreakpointValue({base: false, lg: true});
+
+    return (
+        <Flex
+            as="nav"
+            alignItems="center"
+            justifyContent="space-between"
+            backgroundColor={"#05001a"}
+            boxShadow="md"
+            padding="2"
+            position="fixed"
+            top="0"
+            left="0"
+            right="0"
+            zIndex="sticky"
+            paddingY={10}
+            width="100%"
+            maxWidth={{base: "95%", lg: "70%"}}
+            mx="auto"
+        >
+            <Heading className={quicksand.className} color={"#F2B6A0"}>
+                Tirthya Kamal Dasgupta
+            </Heading>
+            {displayDesktop && (
+                <ButtonGroup gap="2">
+                    {items.map((item) => (
+                        <Button key={item} variant="ghost"
+                                color={"inherit"}
+                                _hover={{color: "black", backgroundColor: "gray.50"}}>
+                            {item}
+                        </Button>
+                    ))}
+                </ButtonGroup>
+            )}
+            {!displayDesktop && (
+                <>
+                    <Spacer/>
+                    <IconButton
+                        aria-label="Open menu"
+                        icon={<HamburgerIcon/>}
+                        onClick={onOpen}
+                        variant="ghost"
+                    />
+                    <Drawer placement="right" onClose={onClose} isOpen={isOpen}>
+                        <DrawerOverlay/>
+                        <DrawerContent backgroundColor={"#05001a"}>
+                            <DrawerBody>
+                                {items.map((item) => (
+                                    <Button
+                                        key={item}
+                                        variant="ghost"
+                                        color={"inherit"}
+                                        _hover={{color: "black", backgroundColor: "gray.50"}}
+                                        w="100%"
+                                        mb={2}
+                                        onClick={onClose}
+                                        colorScheme="teal"
+                                    >
+                                        {item}
+                                    </Button>
+                                ))}
+                            </DrawerBody>
+                        </DrawerContent>
+                    </Drawer>
+                </>
+            )}
+        </Flex>
+    );
+};
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -9,16 +9,22 @@ import Blogs from "./components/client/Blogs";
 import Connect from "./components/client/Connect";
 import Footer from "./components/client/Footer";
 import Navbar from "./components/client/Navbar";
+import NewNavbar from "./components/client/NewNavbar";
 
 export default function Home() {
     return (
         <>
-            <Flex align="center" justify="center" marginY={10}>
+            {/* <Flex align="center" justify="center" marginY={10}>
                 <Box width={{base: "95%", lg: "70%"}}>
                     <Navbar/>
                 </Box>
+            </Flex> */}
+            <Flex>
+                <Box width={{base: "95%", lg: "70%"}}>
+                    <NewNavbar/>
+                </Box>
             </Flex>
-            <Flex align="center" justify="center" marginY={10}>
+            <Flex align="center" justify="center" marginTop={120} marginBottom={10}>
                 <Box width={{base: "95%", lg: "70%"}}>
                     <ProfileIntroduction/>
                 </Box>
@@ -48,7 +54,7 @@ export default function Home() {
                     <Connect/>
                 </Box>
             </Flex>
-            <Flex align="center" justify="center" marginY={20}>
+            <Flex align="center" justify="center" marginTop={20} marginBottom={10}>
                 <Box width={{base: "95%", lg: "70%"}}>
                     <Footer/>
                 </Box>
