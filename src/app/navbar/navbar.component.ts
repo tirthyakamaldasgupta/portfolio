c23e91d31078158f13e666912510062485af2391
tirthyakamaldasgupta <dasguptatirthyakamal@gmail.com>
2024-03-19 15:50:46 +0000

    feat: Declared navbar items in NavbarComponent class. style: Changed global font to Roboto.

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -1,12 +1,35 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-navbar',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './navbar.component.html',
-  styleUrl: './navbar.component.css'
+  styleUrl: './navbar.component.css',
 })
 export class NavbarComponent {
-
+  @Input() brandName: string = 'Tirthya Kamal Dasgupta';
+  @Input() links: { label: string; path: string }[] = [
+    {
+      label: 'Experience',
+      path: '/experience',
+    },
+    {
+      label: 'Projects',
+      path: '/projects',
+    },
+    {
+      label: 'Education',
+      path: '/education',
+    },
+    {
+      label: 'Blogs',
+      path: '/blogs',
+    },
+    {
+      label: 'Connect',
+      path: '/connect',
+    },
+  ];
 }
