e0bec9e6cc5c30d64f61d316fe3b6990ec45a909
Tirthya Kamal Dasgupta <dasguptatirthyakamal@gmail.com>
2024-03-20 10:12:02 +0000

    feat: Configured timeline for work experience inside the experience component.

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,8 @@
 import { Routes } from '@angular/router';
 import { IndexComponent } from './index/index.component';
+import { ExperienceComponent } from './experience/experience.component';
 
 export const routes: Routes = [
   { path: '', component: IndexComponent },
+  { path: 'experience', component: ExperienceComponent },
 ];
---
+++ src/app/experience/experience.component.ts
@@ -0,0 +1,93 @@
+import { CommonModule } from '@angular/common';
+import { Component } from '@angular/core';
+
+interface Description {
+  description: string;
+}
+
+interface Designation {
+  designation: string;
+  startDate: string;
+  endDate: string;
+  duration: string;
+  location: string;
+  model: 'In-office' | 'Hybrid' | 'Remote';
+  descriptions: Description[];
+}
+
+interface Company {
+  name: string;
+  website: string;
+  mode: 'Full-time' | 'Contract';
+  duration: string;
+  designations: Designation[];
+}
+
+@Component({
+  selector: 'app-experience',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './experience.component.html',
+  styleUrl: './experience.component.css',
+})
+export class ExperienceComponent {
+  pageHeading: String = 'Experience';
+  companies: Company[] = [
+    {
+      name: 'BAAR Technologies',
+      website: 'https://baar.ai/',
+      mode: 'Full-time',
+      duration: '3 yrs',
+      designations: [
+        {
+          designation: 'Senior Automation Engineer',
+          startDate: 'Mar 2022',
+          endDate: 'Present',
+          duration: '2 yrs 1 month',
+          location: 'Kolkata, West Bengal, India',
+          model: 'Hybrid',
+          descriptions: [
+            {
+              description:
+                'I am leading the end-to-end development of the backend for a data visualisation platform, constructing it from scratch. My ongoing responsibilities encompass both the development and support of this application, serving two distinct clients. The backend has been developed using Express.js, featuring PostgreSQL as the database, and leveraging Sequelize ORM for data querying and manipulation.',
+            },
+            {
+              description:
+                'I contributed to the development of ULM (User Lifecycle Management) workflows in BAAR, facilitating seamless integration with BAARIGA (automated identity management platform). I successfully integrated HR platforms, including Zoho People and SAP SuccessFactors, along with proprietary HRMS platforms used by financial institutions. I also orchestrated integrations with applications like DocuSign, Jira Service Management, and Okta, along with proprietary applications used by financial institutions, for automated user provisioning through BAARIGA. I developed Python scripts using public APIs and employed Web Automation with Selenium and Playwright for systems lacking public APIs.',
+            },
+            {
+              description:
+                'I have developed a web service using Django and Django REST Framework, functioning as a proxy for interacting with various REST or SOAP APIs. This service hits these APIs and unifies the response obtained from those APIs into a structured JSON format. Additionally, it offers the flexibility to customize the response further by renaming fields and flattening nested structures as needed. I also integrated this web service into several workflows developed in BAAR.',
+            },
+            {
+              description:
+                'I devised and implemented automated workflows in BAAR for streamlined component inspection report generation at Tata Steel. Authored Python code to parse dynamically generated YAML configurations, defining document structure and data, and leveraged the python-docx library to generate precise Word documents.',
+            },
+          ],
+        },
+        {
+          designation: 'Automation Engineer',
+          startDate: 'Apr 2021',
+          endDate: 'Mar 2022',
+          duration: '1 yr',
+          location: 'Kolkata, West Bengal, India',
+          model: 'Remote',
+          descriptions: [
+            {
+              description:
+                'For PwC Middle East (outsourced through TASC Outsourcing), I developed and deployed a 3rd party web application for an original application called Vincere, a recruitment agency software for staffing firms. The client required a simpler interface for posting jobs and viewing the status of the posted jobs. To achieve this, I used HTML, CSS, and jQuery in the frontend, which interacted with the REST APIs of the backend built using Django, Django REST Framework, and MySQL as a database. I leveraged the public APIs provided by Vincere and integrated my application with their public APIs. I deployed the application on an AWS EC2 instance.',
+            },
+            {
+              description:
+                'I developed an RPA Workflow within BAAR for a client, specialised in developing defence electronic products, optimizing the quotation process with the suppliers. I also automated the updation of the quotations of the final products on the company website in accordance with pricing agreements. The workflow included monitoring emails, extracting quotations, conducting calculations with Python scripts, and maintaining continuous communication between the client and vendors until a final agreement was reached. I also employed Web Automation using Robot Framework for updating product quotations on the company website.',
+            },
+            {
+              description:
+                'I played a key role in developing Python scripts for web automation tasks, including form-fill operations, file uploads, general interactions, data extraction, API automations, and data reconciliation. Additionally, I contributed to the creation of plugins for email communication and integration with Microsoft Office desktop applications. My responsibilities also involved handling data extraction from Mainframe computers and PDFs, as well as generating HTML email bodies and charts using Python for various projects, both as part of PoCs and client projects.',
+            },
+          ],
+        },
+      ],
+    },
+  ];
+}
