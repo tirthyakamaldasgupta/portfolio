83d2f3448adb181ee6e862c8b3fc1c3767ffe059
tirthyakamaldasgupta <dasguptatirthyakamal@gmail.com>
2024-12-17 15:55:34 +0000

    feat: Initial commit.

---
+++ src/app/app.routes.ts
@@ -0,0 +1,10 @@
+import { Routes } from '@angular/router';
+import { IndexComponent } from './pages/index/index.component';
+
+export const routes: Routes = [
+  {
+    path: '',
+    component: IndexComponent,
+    title: 'Home - Tirthya Kamal Dasgupta',
+  },
+];
