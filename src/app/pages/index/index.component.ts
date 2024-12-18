b5599f27563e8d05fbaa4a118306e4c5c6c4ea67
tirthyakamaldasgupta <dasguptatirthyakamal@gmail.com>
2024-12-18 00:50:24 +0000

    feat: Initial commit.

--- src/app/pages/index/index.component.ts
+++ src/app/pages/index/index.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-index',
+  standalone: true,
   imports: [],
   templateUrl: './index.component.html',
   styleUrl: './index.component.css'
